682d7c0fa240dcb12f3c59947525e123188f49d1
Krishna Vakte <kvakte02@gmail.com>
2024-05-11 14:15:44 +0530

    add url in env

--- src/context/StroreContext.jsx
+++ src/context/StroreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null)
 const StroeContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = 'http://localhost:4000'
+    const url = String(import.meta.env.VITE_BACKEND_URL);
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
     const [loading, setLoading] = useState(false)
